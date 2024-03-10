d3b38ba776c67b74112fc81ad7d164d6b746ead0
Not_yq <s10221789@connect.np.edu.sg>
2024-03-11 01:49:08 +0800

    capsolver function fully implemented

--- captchaSolver.py
+++ captchaSolver.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
 import requests
 import capsolver
+import datetime
 
 
 # leave window open when done
@@ -13,54 +14,100 @@ options.add_argument('--user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\C
 options.add_experimental_option("detach", True)
 driver = uc.Chrome()
 
-websiteURL = 'https://nopecha.com/demo/recaptcha#moderate'
-iframe = driver.find_element(By.XPATH('/html/body/div[1]/div/div[2]'))
-captcha_site_key = iframe.get_attribute('data-sitekey')
-print(captcha_site_key)
+websiteURL = 'https://www.google.com/recaptcha/api2/demo'
+driver.get(websiteURL)
+driver.maximize_window()
+wait = WebDriverWait(driver, 10)
+print("Opened website")
+# iframe = driver.find_element(By.XPATH('/html/body/div[1]/div/div[2]'))
+# captcha_site_key = iframe.get_attribute('data-sitekey')
+# print(captcha_site_key)
+captcha_element = driver.find_element(By.CLASS_NAME, "g-recaptcha")
+data_sitekey = captcha_element.get_attribute("data-sitekey")
+print(data_sitekey)
+
+def main():
+    # Solve CAPTCHA
+    driver.get(websiteURL)
+    wait = WebDriverWait(driver, 10)
+    driver.maximize_window()
+    token = get_token(websiteURL, data_sitekey)
+    post_data(token, websiteURL)
+    print("Submitted form")
 
 
-# def main():
-#     # Solve CAPTCHA
-#     driver.get(websiteURL)
-#     solve_captcha(websiteURL, websiteKey)
-#     driver.maximize_window()
-#     wait = WebDriverWait(driver, 10)
 
-# def solve_captcha(websiteURL, websiteKey):
-#     # Your CapSolver API key
-#     capsolver.api_key = 'CAP-8E7E60DF49320EA19B99D3EDEF3FF155'
+def get_token(websiteURL, websiteKey):
+    # Your CapSolver API key
+    capsolver.api_key = 'CAP-8E7E60DF49320EA19B99D3EDEF3FF155'
 
-#     # # CapSolver API endpoint
-#     capSolver_api_url = 'https://api.capsolver.com/createTask'
+    # Parameters for the reCAPTCHA v2 (proxyless) task
+    solution = capsolver.solve({
+        "type": "ReCaptchaV2TaskProxyLess",
+        "websiteURL": websiteURL,
+        "websiteKey": websiteKey
+    })
 
-#     # Parameters for the reCAPTCHA v2 (proxyless) task
-#     solution = capsolver.solve({
-#         "type": "ReCaptchaV2TaskProxyLess",
-#         "websiteURL": websiteURL,
-#         "websiteKey": websiteKey
-#     })
+    print('CAPTCHA solution:', solution)
+    
+    token = solution['gRecaptchaResponse']
 
-#     print('CAPTCHA solution:', solution)
 
-#     # # Send HTTP POST request to CapSolver API
-#     # response = requests.post(capSolver_api_url, data=params)
 
-#     # # Check if request was successful
-#     # if response.status_code == 200:
-#     #     # Parse response JSON
-#     #     response_json = response.json()
+    # payload = {
+    #     'g-recaptcha-response': token,
+    #     # Include any other necessary data
+    # }
 
-#     #     # Check if solution is available
-#     #     if response_json['status'] == 1:
-#     #         # Solution found
-#     #         solution = response_json['solution']
-#     #         print('CAPTCHA solution:', solution)
-#     #     else:
-#     #         # No solution found or error occurred
-#     #         error_message = response_json['error']
-#     #         print('Error:', error_message)
-#     # else:
-#     #     # Request failed
-#     #     print('Failed to connect to CapSolver API')
+    # # Send a POST request to the endpoint with the token in the payload
+    # solved_captcha = requests.post(websiteURL, data=payload)
 
-# main()
+    # print("solved captcha: ", solved_captcha)
+
+    return token
+
+def post_data(token, websiteURL):
+    driver.get(websiteURL)
+    # Find the reCAPTCHA form field (replace 'g-recaptcha-response' with the actual field ID)
+    recaptcha_response_field = driver.find_element(By.ID, 'g-recaptcha-response')
+    # Inject the token into the field
+    driver.execute_script("arguments[0].value = arguments[1]", recaptcha_response_field, token)
+    # Submit the form
+    form = driver.find_element(By.ID, "recaptcha-demo-submit")
+    form.submit()
+
+
+# def post_data(token, websiteURL):
+#     cookies = {
+#         '_ga': 'GA1.1.917993207.1709800797',
+#         '_ga_70K7MBE4SF': 'GS1.1.1709803121.2.1.1709803122.59.0.0',
+#     }
+
+#     headers = {
+#         'authority': 'nopecha.com',
+#         'accept': '*/*',
+#         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+#         'content-type': 'application/json',
+#         # 'cookie': '_ga=GA1.1.917993207.1709800797; _ga_70K7MBE4SF=GS1.1.1709803121.2.1.1709803122.59.0.0',
+#         'origin': 'https://nopecha.com',
+#         'referer': 'https://nopecha.com/demo/recaptcha',
+#         'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+#         'sec-ch-ua-mobile': '?0',
+#         'sec-ch-ua-platform': '"Windows"',
+#         'sec-fetch-dest': 'empty',
+#         'sec-fetch-mode': 'cors',
+#         'sec-fetch-site': 'same-origin',
+#         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+#     }
+
+#     json_data = {
+#         'token': token,
+#         'type': 'moderate',
+#     }
+
+#     response = requests.post(websiteURL, cookies=cookies, headers=headers, json=json_data)
+#     print(response)
+
+#     return response
+
+main()
